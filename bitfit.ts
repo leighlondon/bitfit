98afe189ed49edd2dcd12a7d5dd95cf6111d2a62
Leigh London <leigh.00.london@gmail.com>
2018-04-29 19:16:27 +1000

    func: helpers

--- bitfit.ts
+++ bitfit.ts
@@ -12,3 +12,18 @@ export namespace Fitbit {
     readonly newValue: string
   }
 }
+
+export const stripQuotes = (str: string): string => {
+  return str ? str.replace(/"/g, "") : ""
+}
+
+/**
+ * currentDate returns the current date in YYYY-MM-DD format.
+ */
+export const currentDate = (): string => {
+  let date = new Date()
+  let yyyy = date.getFullYear()
+  let mm = date.getMonth() + 1
+  let dd = date.getDate()
+  return `${yyyy}-${mm}-${dd}`
+}
