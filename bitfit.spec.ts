7cfee934701d657ad6c4aa83aebd49944f295eba
Leigh London <leigh.00.london@gmail.com>
2018-05-07 11:08:29 +1000

    spec: date testing

--- bitfit.spec.ts
+++ bitfit.spec.ts
@@ -1,8 +1,35 @@
-import { Fitbit, receiver, stripQuotes } from "./bitfit"
 import { expect } from "chai"
-import "mocha"
+import { describe, it } from "mocha"
+
+import {
+  Fitbit,
+  currentDate,
+  formatDate,
+  receiver,
+  stripQuotes
+} from "./bitfit"
 
 describe("Helper", () => {
+  describe("currentDate", () => {
+    it("returns concrete value", () => {
+      let current = currentDate()
+      expect(current).not.to.be.undefined
+    })
+
+    it("returns the right format", () => {
+      let current = currentDate()
+      expect(current).to.match(/^\d{4}\-\d{2}\-\d{2}$/)
+    })
+  })
+
+  describe("formatDate", () => {
+    it("pads correctly", () => {
+      let date: Date = new Date(1901, 1, 1)
+      let formatted = formatDate(date)
+      expect(formatted.length).to.equal(10)
+    })
+  })
+
   describe("stripQuotes", () => {
     it("removes quotes", () => {
       const s = '"a"'
@@ -39,6 +66,7 @@ describe("Interface", () => {
       expect(a).to.equal(b)
     })
   })
+
   describe("SettingEvent", () => {
     it("has key, newValue, oldValue", () => {
       let a = { key: "key", newValue: "newValue", oldValue: "oldValue" }
