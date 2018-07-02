7e719fade0c53e396fe24216047292c864a923b3
Leigh London <leigh.00.london@gmail.com>
2018-07-02 20:05:47 +1000

    lint: shuffle shuffle shuffle

--- bitfit.spec.ts
+++ bitfit.spec.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai"
-import { describe, it } from "mocha"
+import { expect } from "chai";
+import { describe, it } from "mocha";
 
 import {
   Fitbit,
@@ -7,71 +7,71 @@ import {
   formatDate,
   receiver,
   stripQuotes
-} from "./bitfit"
+} from "./bitfit";
 
 describe("Helper", () => {
   describe("currentDate", () => {
     it("returns concrete value", () => {
-      let current = currentDate()
-      expect(current).not.to.be.undefined
-    })
+      let current = currentDate();
+      expect(current).not.to.be.undefined;
+    });
 
     it("returns the right format", () => {
-      let current = currentDate()
-      expect(current).to.match(/^\d{4}\-\d{2}\-\d{2}$/)
-    })
-  })
+      let current = currentDate();
+      expect(current).to.match(/^\d{4}\-\d{2}\-\d{2}$/);
+    });
+  });
 
   describe("formatDate", () => {
     it("pads correctly", () => {
-      let date: Date = new Date(1901, 1, 1)
-      let formatted = formatDate(date)
-      expect(formatted.length).to.equal(10)
-    })
-  })
+      let date: Date = new Date(1901, 1, 1);
+      let formatted = formatDate(date);
+      expect(formatted.length).to.equal(10);
+    });
+  });
 
   describe("stripQuotes", () => {
     it("removes quotes", () => {
-      const s = '"a"'
-      expect(stripQuotes(s)).to.equal("a")
-    })
-  })
+      const s = '"a"';
+      expect(stripQuotes(s)).to.equal("a");
+    });
+  });
 
   describe("receiver", () => {
     it("routes as key and value arguments", () => {
-      let key: string = ""
-      let value: string = ""
+      let key: string = "";
+      let value: string = "";
       let msg: Fitbit.MessageEvent = {
         data: { key: "key", newValue: "value" }
-      }
+      };
       // a trivial router to store the data in scope.
       let router = (k, v) => {
-        key = k
-        value = v
-      }
+        key = k;
+        value = v;
+      };
       // mount like the real code, and test the values.
-      let onmessage = receiver(router)
-      onmessage(msg)
-      expect(key).to.equal("key")
-      expect(value).to.equal("value")
-    })
-  })
-})
+      let onmessage = receiver(router);
+      onmessage(msg);
+      expect(key).to.equal("key");
+      expect(value).to.equal("value");
+    });
+  });
+});
 
 describe("Interface", () => {
   describe("UpdateEvent", () => {
     it("has key, newValue", () => {
-      let a = { key: "key", newValue: "newValue" }
-      let b: Fitbit.UpdateEvent = a
-      expect(a).to.equal(b)
-    })
-  })
+      let a = { key: "key", newValue: "newValue" };
+      let b: Fitbit.UpdateEvent = a;
+      expect(a).to.equal(b);
+    });
+  });
 
   describe("SettingEvent", () => {
     it("has key, newValue, oldValue", () => {
-      let a = { key: "key", newValue: "newValue", oldValue: "oldValue" }
-      let b: Fitbit.SettingEvent = a
-      expect(a).to.equal(b)
-    })
-  })
-})
+      let a = { key: "key", newValue: "newValue", oldValue: "oldValue" };
+      let b: Fitbit.SettingEvent = a;
+      expect(a).to.equal(b);
+    });
+  });
+});
--- bitfit.ts
+++ bitfit.ts
@@ -1,21 +1,21 @@
 export namespace Fitbit {
   export interface MessageEvent {
-    readonly data: UpdateEvent
+    readonly data: UpdateEvent;
   }
   export interface UpdateEvent {
-    readonly key: string
-    readonly newValue: string
+    readonly key: string;
+    readonly newValue: string;
   }
   export interface SettingEvent {
-    readonly key: string
-    readonly oldValue: string
-    readonly newValue: string
+    readonly key: string;
+    readonly oldValue: string;
+    readonly newValue: string;
   }
 }
 
 export const stripQuotes = (str: string): string => {
-  return str ? str.replace(/"/g, "") : ""
-}
+  return str ? str.replace(/"/g, "") : "";
+};
 
 /**
  * Pads a number to two digits and converts to a string.
@@ -23,24 +23,24 @@ export const stripQuotes = (str: string): string => {
  * @param i the number to be padded
  */
 export const zeroPad = (i: number): string => {
-  return i < 10 ? "0" + i : String(i)
-}
+  return i < 10 ? "0" + i : String(i);
+};
 
 /**
  * currentDate returns the current date in YYYY-MM-DD format.
  */
 export const currentDate = (): string => {
-  return formatDate(new Date())
-}
+  return formatDate(new Date());
+};
 
 export const formatDate = (d: Date): string => {
-  let yyyy = d.getFullYear()
-  let month = d.getMonth() + 1
-  let date = d.getDate()
-  let mm: string = month < 10 ? zeroPad(month) : String(month)
-  let dd: string = date < 10 ? zeroPad(date) : String(date)
-  return `${yyyy}-${mm}-${dd}`
-}
+  let yyyy = d.getFullYear();
+  let month = d.getMonth() + 1;
+  let date = d.getDate();
+  let mm: string = month < 10 ? zeroPad(month) : String(month);
+  let dd: string = date < 10 ? zeroPad(date) : String(date);
+  return `${yyyy}-${mm}-${dd}`;
+};
 
 /**
  * Creates a receiver to handle updates from the peer socket, which will
@@ -53,9 +53,9 @@ export const receiver = (
   router: (key: string, value: string) => void
 ): ((event: Fitbit.MessageEvent) => void) => {
   const handler = (event: Fitbit.MessageEvent) => {
-    let key = event.data.key
-    let value = stripQuotes(event.data.newValue)
-    router(key, value)
-  }
-  return handler
-}
+    let key = event.data.key;
+    let value = stripQuotes(event.data.newValue);
+    router(key, value);
+  };
+  return handler;
+};
